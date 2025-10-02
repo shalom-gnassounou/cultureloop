062a891a4bdfb074b30bfe3d6acaf170014340cb
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-02 15:40:15 +0200

    add of the connexion and clean of the main dart

---
+++ lib/connexion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Connexion extends StatelessWidget {
+  const Connexion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Container(color: Colors.blue,)));
+  }
+}
