92782cdfd3d5aa4722e7b74f8f41a9e3f51cf8e9
mumu69009 <murielle.vergnais@labom2iformation.fr>
2025-10-03 16:54:50 +0200

    Modif header

---
+++ lib/widgets/header_section.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
