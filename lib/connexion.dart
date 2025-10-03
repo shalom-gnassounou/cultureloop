5da3992b4eebde1f6093a7d5b09ad761635ae20c
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-03 14:23:23 +0200

    local_auth function

--- lib/connexion.dart
+++ lib/connexion.dart
@@ -1,4 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+class AuthFinger extends StatefulWidget {
+  const AuthFinger({super.key});
+
+  @override
+  State<AuthFinger> createState() => _AuthFingerState();
+}
+
+class _AuthFingerState extends State<AuthFinger> {
+  final LocalAuthentication authentication = LocalAuthentication();
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await authentication.authenticate(
+        localizedReason: "Veuillez vous identifiez avec votre empreinte",
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+          stickyAuth: true,
+        ),
+      );
+    } catch (errors) {
+      print("erreurs: $errors");
+    }
+    if (authenticated) {
+      print("Authentification réussie !");
+      // Navigator.pushReplacement(...);
+    } else {
+      print("echec");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
+
 
 class Connexion extends StatelessWidget {
   const Connexion({super.key});
@@ -28,19 +70,18 @@ class Connexion extends StatelessWidget {
                   fontWeight: FontWeight.w800,
                 ),
               ),
-              SizedBox(height: 60,),
+              SizedBox(height: 60),
               ElevatedButton.icon(
-                  onPressed : () {
-                    //local_auth function
-                  },
-                  icon: Icon(Icons.fingerprint,size:30),
-                  label: Text("conexion avec l'empreinte"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                  )
-                    )
+                onPressed:(){
 
+                },
+                icon: Icon(Icons.fingerprint, size: 30),
+                label: Text("conexion avec l'empreinte"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                ),
+              ),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Connexion()
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
 
     );
   }
