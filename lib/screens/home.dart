03b3fcb4b1e9bccd467dcbe2b31304148f9d090d
mumu69009 <murielle.vergnais@labom2iformation.fr>
2025-10-04 01:00:49 +0200

    Add category and category item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
+import 'package:cultureloop/widgets/category.dart';
+import 'package:cultureloop/widgets/header_section.dart';
 import 'package:flutter/material.dart';
-
 import '../widgets/navigation_bar_app.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +9,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child:Column(
+          children: [
+            HeaderSection(),
+            CategorySection()
+          ],
+        )
+     ),
       bottomNavigationBar: NavigationBarApp(),
     );
   }
