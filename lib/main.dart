6a7aabaea7b1bb3dd5abb6b157b16119a03024b1
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-03 16:58:28 +0200

    change

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Culture Loop',
-     home: LoginPage(),
+     home: HomePage(),
     );
   }
 }
