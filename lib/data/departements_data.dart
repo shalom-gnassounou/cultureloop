2dc8afc0b1da00c59b7fc3ce3b4cb0231d4af127
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-08 10:48:36 +0200

    add of the data and model

---
+++ lib/data/departements_data.dart
@@ -0,0 +1,9 @@
+ import 'package:cultureloop/service/model/departement.dart';
+
+ final List<Departement> departements = [
+  Departement(id:5 , name: "Africa Art"),
+  Departement(id:6 , name: "Asian Art"),
+  Departement(id:11 , name: "European Pantings"),
+  Departement(id:14 , name: "Islamic Art"),
+  Departement(id:13 , name: "Greek and Roman Art"),
+];
