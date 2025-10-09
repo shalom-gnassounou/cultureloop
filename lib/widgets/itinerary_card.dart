8ef05ab9c492ce8f27fbce738b2c3e18200d9cd5
Murielle Vergnais <muriellevergnais@Host-003.lan>
2025-10-09 14:17:02 +0200

    Add page itinerary

---
+++ lib/widgets/itinerary_card.dart
@@ -0,0 +1,11 @@
+class ItineraryCard {
+  final String name;
+  final String department;
+  final String imageUrl;
+
+  ItineraryCard({
+    required this.name,
+    required this.department,
+    required this.imageUrl,
+  });
+}
