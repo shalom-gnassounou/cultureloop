e988d17e83c446a8bc4f915155d86a5c6c09b5ce
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-10-14 09:42:34 +0200

    model of the artwork

---
+++ lib/service/model/art_work.dart
@@ -0,0 +1,14 @@
+class ArtWork {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String artist;
+  final String date;
+
+  ArtWork(
+      {required this.id, required this.title, required this.imageUrl, required this.artist, required this.date})
+   factory
+
+
+
+}
